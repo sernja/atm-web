5284491c85dc2d776b26891d628ec9b06484ef9c
sern <sanghit@hotmail.com>
2020-09-27 22:24:27 +0700

    db to jpa

--- atm/src/main/java/th/ac/ku/atm/data/CustomerRepository.java
+++ atm/src/main/java/th/ac/ku/atm/data/CustomerRepository.java
@@ -1,39 +1,10 @@
 package th.ac.ku.atm.data;
 
-
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import th.ac.ku.atm.model.Customer;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
 @Repository
-public class CustomerRepository {
-    public List<Customer> findAll() {
-        return null;
-    }
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
-    public Customer findById(int id) {
-        return null;
-    }
-
-    public void save(Customer customer) { }
-
-    public void deleteById(int id) { }
-    class CustomerMapper implements RowMapper<Customer> {
-
-        @Override
-        public Customer mapRow(ResultSet resultSet, int i)
-                throws SQLException {
-
-            int id = resultSet.getInt("id");
-            String name = resultSet.getString("name");
-            String pin = resultSet.getString("pin");
-
-            Customer customer = new Customer(id, name, pin);
-            return customer;
-        }
-    }
 }
